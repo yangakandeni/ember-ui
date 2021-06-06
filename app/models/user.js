f3a7f3a9596a2de93bc579a76487052764c346a2
yangakandeni <kandeni.yanga@gmail.com>
2021-06-06 23:02:05 +0200

    Added value attribute

--- app/models/user.js
+++ app/models/user.js
@@ -3,4 +3,7 @@ import Model, { attr } from '@ember-data/model';
 export default class UserModel extends Model {
   @attr('string') name;
   @attr('string') image;
+
+  // added value attribute (which is a property of the "server" data)
+  @attr('boolean') value;
 }
