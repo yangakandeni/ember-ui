30052d288211fe2ce3c63d1e66ed611dd8755975
yangakandeni <kandeni.yanga@gmail.com>
2021-06-07 02:35:25 +0200

    updated model tests

--- tests/unit/models/user-test.js
+++ tests/unit/models/user-test.js
@@ -2,17 +2,24 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import { run } from '@ember/runloop';
 
-module('Unit | Model | user', function(hooks) {
+module('Unit | Model | user', function (hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('it should change value', function (assert) {
     let store = this.owner.lookup('service:store');
-    let userModel = store.createRecord('user', {});
+    let user = store.createRecord('user', {
+      id: 1,
+      name: 'Kendrick Lamar',
+      image: 'https://en.wikipedia.org/wiki/File:Pulitzer2018-portraits-kendrick-lamar.jpg',
+      value: false
+    })
 
-    assert.equal(userModel.value, false, 'Default value should be false.')
-    run(() => userModel.changeValue());
-    assert.equal(userModel.value, true, 'Value should change when function is called.');
-    assert.ok(userModel);
+    assert.equal(user.value, false, 'Default value should be false.')
+    assert.equal(user.id, 1)
+    assert.equal(user.name, "Kendrick Lamar")
+    assert.equal(user.image, "https://en.wikipedia.org/wiki/File:Pulitzer2018-portraits-kendrick-lamar.jpg")
+    run(() => user.changeValue());
+    assert.equal(user.value, true, 'Value should change when function is called.');
+    assert.ok(user);
   });
 });
